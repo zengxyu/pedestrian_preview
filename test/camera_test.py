a903f841eb8515e33c725d672f73ee83ee2255db
zeng <s6xizeng@uni-bonn.de>
2023-01-06 11:46:45 +0800

    delete files

--- test/camera_test.py
+++ test/camera_test.py
@@ -1,13 +1,42 @@
+import time
+
+import numpy as np
 import pybullet as p
 import pybullet_data
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
-p.setGravity(0,0,-10)
+
+from environment.robots.human import Human, Man
+
+
+def reset_human(human, distance, robot_angle, human_angle, gait_phase):
+    human.reset()
+    x = distance * np.cos(-np.pi / 2 - robot_angle)
+    y = distance * np.sin(-np.pi / 2 - robot_angle)
+    orientation = -np.pi / 2 - robot_angle + human_angle
+    human.resetGlobalTransformation(
+        xyz=np.array([x, y, 0.94 * human.scaling]),
+        rpy=np.array([0, 0, orientation - np.pi / 2]),
+        gait_phase_value=0
+    )
+
+
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # used by loadURDF
+p.setGravity(0, 0, -10)
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
-cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
+human = Man(physicsClient)
+human.reset()
+human.resetGlobalTransformation(
+    xyz=np.array([0, 0, 0.94 * human.scaling]),
+    rpy=np.array([0, 0, 0]),
+    gait_phase_value=0
+)
 p.stepSimulation()
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos,cubeOrn)
+for i in range(1000000000000000000000):
+    cubePos, cubeOrn = p.getBasePositionAndOrientation(human.body_id)
+    cubePos = np.array(cubePos)
+    # xyz, quaternion = p.invertTransform(cubePos, cubeOrn)
+    time.sleep(0.01)
+    human.advance(np.array([0, 0, 0]), np.array([0, 0, 0, 1]))
+    p.stepSimulation()
+
 p.disconnect()
