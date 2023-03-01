1a64d5bb6523a347538682936220671fc6631fa8
xiao-hua-sheng <1345795357@qq.com>
2023-03-01 18:37:46 +0800

    更新參數

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ from warnings import filterwarnings
 from environment.environment_bullet import EnvironmentBullet
 
 filterwarnings(action='ignore', category=DeprecationWarning, message='`np.')
-set_random_seeds(1111)
+set_random_seeds(1221)
 args = process_args()
 
 if args.render:
