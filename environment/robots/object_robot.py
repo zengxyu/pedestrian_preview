d1db0fe2ec141194fac9f0be3e95fb839540c023
zeng <s6xizeng@uni-bonn.de>
2023-03-04 09:55:10 +0800

    add draw picture

--- environment/robots/object_robot.py
+++ environment/robots/object_robot.py
@@ -52,14 +52,14 @@ class ObjectRobot(BaseRobot):
 
         # 障碍物的高度要比0.5高，不然探测不到
         begin = [cur_position[0] + x0, cur_position[1] + y0]
-        ray_begins = [(begin[0], begin[1], 0.5)]
+        ray_begins = [(begin[0], begin[1], 0.2)]
 
         # 探测终点
         distance = 10  # 探测距离
-        x1 = (s0 + distance) * np.cos(ray_theta)
-        y1 = (s0 + distance) * np.sin(ray_theta)
+        x1 = (distance) * np.cos(ray_theta)
+        y1 = (distance) * np.sin(ray_theta)
 
-        ray_ends = [(begin[0] + x1, begin[1] + y1, 0.5)]
+        ray_ends = [(begin[0] + x1, begin[1] + y1, 0.2)]
 
         # 调用激光探测函数
         results = self.p.rayTestBatch(ray_begins, ray_ends)
@@ -80,24 +80,17 @@ class ObjectRobot(BaseRobot):
         theta = np.arctan2(delta_y, delta_x)
         hit_distance = self.compute_hit_distance(cur_position, theta)
         # 发射另外两根射线(robot 半径)
-        # delta_theta = np.arctan2(self.radius, hit_distance-self.radius)
-        # theta1 = theta + delta_theta
-        # theta2 = theta - delta_theta
-        # hit_distance1 = self.compute_prob(cur_position, theta1)
-        # hit_distance2 = self.compute_prob(cur_position, theta2)
-        # min_distance = min(hit_distance, hit_distance1, hit_distance2)
         intent_distance = np.linalg.norm([delta_x, delta_y])
 
         # 如果探测到的障碍物距离小于意图移动的距离
-        if hit_distance - 0.25 < intent_distance:
+        if hit_distance - self.radius - 0.1 < intent_distance:
             ratio = hit_distance / intent_distance
             delta_x = 0
             delta_y = 0
         else:
             print()
-        # pose = np.array([*cur_position, np.arctan2(delta_y, delta_x)])
-        # self.debug_line_id = plot_robot_direction_line(self.p, self.debug_line_id, current_pose=pose, color=[0, 0, 1])
-        # plot_lidar_ray(self._bullet_client, results, rayFroms, rayTos, missRayColor=[1, 0, 0], hitRayColor=[0, 0, 1])
+        pose = np.array([*cur_position, np.arctan2(delta_y, delta_x)])
+        self.debug_line_id = plot_robot_direction_line(self.p, self.debug_line_id, current_pose=pose, color=[0, 0, 1])
         return delta_x, delta_y
 
     def small_step_pose_control(self, delta_x_, delta_y_, delta_yaw):
